578158784031b897d8dfe44a21c1d88122346bd5
Joshua M. Clulow <jmc@oxide.computer>
2021-04-08 17:50:55 -0700

    [helios] smartmontools distro name fix

--- build/smartmontools/build.sh
+++ build/smartmontools/build.sh
@@ -43,7 +43,7 @@ CONFIGURE_OPTS="
     --with-scriptpath=$USRBIN:$OOCEBIN
 "
 
-MAKE_ARGS_WS="BUILD_INFO='\"(OmniOS r$RELVER)\"'"
+MAKE_ARGS_WS="BUILD_INFO='\"(Helios r$RELVER)\"'"
 
 init
 download_source $PROG $PROG $VER
