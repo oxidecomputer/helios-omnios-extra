cec84ba50f05752d86c4875ecf5b2990e1fc7598
Joshua M. Clulow <jmc@oxide.computer>
2021-05-07 13:54:07 -0700

    [helios] radare2: relver fixups

--- build/radare2/build.sh
+++ build/radare2/build.sh
@@ -22,11 +22,6 @@ VER=5.2.1
 SUMMARY="A low-level software forensics tool"
 DESC="$PROG - $SUMMARY"
 
-if [ $RELVER -lt 151034 ]; then
-    logmsg "--- $PKG is not built for r$RELVER"
-    exit 0
-fi
-
 OPREFIX=$PREFIX
 PREFIX+=/$PROG
 
