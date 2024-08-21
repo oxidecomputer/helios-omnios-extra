7487f43d1ca63114da2452327ba0298cb64552f6
Andy Fiddaman <andy@oxidecomputer.com>
2024-08-21 13:05:39 +0100

    go package updates (#18)
    Co-authored-by: Dominik Hassler <hadfl@omnios.org>

--- build/go/build-122.sh
+++ build/go/build-122.sh
@@ -12,13 +12,13 @@
 # http://www.illumos.org/license/CDDL.
 # }}}
 
-# Copyright 2023 OmniOS Community Edition (OmniOSce) Association.
+# Copyright 2024 OmniOS Community Edition (OmniOSce) Association.
 
 . ../../lib/build.sh
 
 PROG=go
-PKG=ooce/developer/go-120
-VER=1.20.5
+PKG=ooce/developer/go-122
+VER=1.22.6
 SUMMARY="The Go Programming Language"
 DESC="An open source programming language that makes it easy to build simple, "
 DESC+="reliable, and efficient software."
