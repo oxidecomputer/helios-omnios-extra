1adcc48a6bfccec5e4f4af3556ad56e19827faa6
Andy Fiddaman <illumos@fiddaman.net>
2025-03-06 16:52:15 +0000

    abseil: install in standard location, build 32-bit and 64-bit variants

--- build/abseil/build.sh
+++ build/abseil/build.sh
@@ -22,25 +22,19 @@ PKG=ooce/library/abseil
 SUMMARY="abseil"
 DESC="Collection of c++ libraries from google"
 
-# The protobuf package installs its own copy of this library in the standard
-# location, so we install into a abseil-specific directory.
-# XXX: see if the protobuf package can be convinced to use a pre-existing
-# abseil, and make it depend on this one
-PREFIX=$PREFIX/absl
+set_builddir $PROG-cpp-$VER
 
-set_arch amd64
-
-CONFIGURE_OPTS[amd64]="
+CONFIGURE_OPTS="
     -DCMAKE_BUILD_TYPE=Release
     -DCMAKE_INSTALL_PREFIX=$PREFIX
 "
 
-set_mirror github.com
-CHECKSUM_VALUE=sha256:e887b423da5a1ba66e71610094fd7147ff2febfedccdfbf00f2c644ac21adf83
-BUILDDIR=abseil-cpp-$VER
+CONFIGURE_OPTS[i386]="-DCMAKE_INSTALL_LIBDIR=$PREFIX/${LIBDIRS[i386]}"
+CONFIGURE_OPTS[amd64]="-DCMAKE_INSTALL_LIBDIR=$PREFIX/${LIBDIRS[amd64]}"
 
 init
-download_source abseil/abseil-cpp/releases/download/${VER} abseil-cpp-${VER}
+download_source abseil $PROG-cpp-$VER
+patch_source
 prep_build cmake
 build -noctf    # C++
 make_package
